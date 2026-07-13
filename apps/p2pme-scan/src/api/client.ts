d473d716004484a5f3d994f189262fbcfd2a45d9
Danny Gonzalez <jesusgalicia03@gmail.com>
2026-07-13 05:47:53 -0300

    asdasd

--- apps/p2pme-scan/src/api/client.ts
+++ apps/p2pme-scan/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    "https://p2p-api.qiqinovels.workers.dev/v1" /* import.meta.env.VITE_API_URL || 'http://localhost:8787/v1', */,
+     import.meta.env.VITE_API_URL || 'http://localhost:8787/v1',,
 });
 
 export default apiClient;
